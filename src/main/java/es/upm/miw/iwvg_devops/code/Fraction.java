5818b45c770bcf10724e93d03f63308e3755138d
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-01-10 16:28:22 -0800

    Added Fraction #6

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -0,0 +1,48 @@
+package es.upm.miw.iwvg_devops.code;
+
+
+public class Fraction {
+
+    private int numerator;
+
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+}
