93ae46804837b9c5ea8599f6f2c9c3b3f788d772
wzzzzwwzw <46055260+wzzzzwwzw@users.noreply.github.com>
2022-01-10 16:46:21 -0800

    Enhanced FractionTest #9

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
@@ -4,7 +4,8 @@ import es.upm.miw.iwvg_devops.code.Fraction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FractionTest {
 
@@ -45,5 +46,36 @@ public class FractionTest {
         assertEquals(1.6, testFraction.decimal(), 10e5);
     }
 
+    @Test
+    void testAdd() {
+        Fraction addedFraction = new Fraction(68, 30);
+        assertTrue(addedFraction.isEquivalent(testFraction.add(inputFraction)));
+    }
+    @Test
+    void testSub() {
+        Fraction SubbedFraction = new Fraction(14, 15);
+        assertTrue(SubbedFraction.isEquivalent(testFraction.sub(inputFraction)));
+    }
+
+    @Test
+    void testIsProper() {
+        assertFalse(testFraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertTrue(testFraction.isImproper());
+    }
+
+    @Test
+    void testDivide() {
+        Fraction dividedFraction = new Fraction(48, 20);
+        assertTrue(dividedFraction.isEquivalent(testFraction.divide(inputFraction)));
+    }
+    @Test
+    void testMul() {
+        Fraction dividedFraction = new Fraction(32, 30);
+        assertTrue(dividedFraction.isEquivalent(testFraction.mul(inputFraction)));
+    }
 
 }
